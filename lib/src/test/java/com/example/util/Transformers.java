72fe6f216ed84d507c6f5bb34cb6648674068275
ksilin <konstantin.siln@gmail.com>
2022-08-20 16:12:46 +0200

    added ValueTransformerWithKey, Trifunction adn timeLoggingValueTransformer

--- lib/src/test/java/com/example/util/Transformers.java
+++ lib/src/test/java/com/example/util/Transformers.java
@@ -2,8 +2,12 @@ package com.example.util;
 
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Transformer;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKey;
+import org.apache.kafka.streams.kstream.ValueTransformerWithKeySupplier;
 import org.apache.kafka.streams.processor.ProcessorContext;
 import org.apache.kafka.streams.state.KeyValueStore;
+
+import java.text.SimpleDateFormat;
 
 public class Transformers {
 
@@ -31,5 +35,48 @@ public class Transformers {
             }
         };
         return transformer;
+    }
+
+    private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+
+    public static  <K, V> ValueTransformerWithKeySupplier<K, V, V> timeLoggingValueTransformerSupplier(String name ){
+        return () -> timeLoggingValueTransformer(name);
+    }
+
+    public static <K, V> ValueTransformerWithKey<K, V, V> timeLoggingValueTransformer(String name ) {
+        return createCtxAwareValueTransformer((k, v, ctx) -> {
+            System.out.println(name + " : processing" + k + " : " + v);
+            System.out.println("task: "+ ctx.taskId());
+            System.out.println("topic: "+ ctx.topic());
+            System.out.println("partition: "+ ctx.partition());
+            System.out.println("offset: "+ ctx.offset());
+            System.out.println("wallclock: " + df.format(ctx.currentSystemTimeMs()));
+            System.out.println("stream:  " +  df.format(ctx.currentStreamTimeMs()));
+            System.out.println("record ts: " + df.format(ctx.timestamp()));
+            return v;
+        });
+    }
+
+
+    public static <K, V, R> ValueTransformerWithKey<K, V, R> createCtxAwareValueTransformer(TriFunction<K, V, ProcessorContext, R> fun) {
+        return new ValueTransformerWithKey<K, V, R >() {
+
+            ProcessorContext ctx;
+
+            @Override
+            public void init(ProcessorContext processorContext) {
+                ctx = processorContext;
+            }
+
+            @Override
+            public R transform(K k, V v) {
+                return fun.apply(k, v, ctx);
+            }
+
+            @Override
+            public void close() {
+            }
+
+        };
     }
 }
